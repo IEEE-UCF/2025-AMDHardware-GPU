c1f41ffb83f5f077b53414b2d649a483f6802a84
BigManRaffa <animedudebro@gmail.com>
2025-08-23 23:26:24 -0400

    god i forgot i needed to fix this for gpu_top lol

--- src/fragment_shader.sv
+++ src/fragment_shader.sv
@@ -1,18 +1,23 @@
 module fragment_shader #(
     parameter int DATA_WIDTH = 32,
-    parameter int VEC_SIZE = 4
+    parameter int VEC_SIZE   = 4,
+    parameter int CORD_WIDTH = 10
 )(
     input logic clk,
     input logic rst_n,
 
     input logic i_frag_valid,
-    input logic [VEC_SIZE-1:0][DATA_WIDTH-1:0] i_frag_color,
-    input logic [1:0][DATA_WIDTH-1:0] i_frag_tex_coord,
+    input logic signed [CORD_WIDTH-1:0]           i_frag_x,
+    input logic signed [CORD_WIDTH-1:0]           i_frag_y,
+    input logic [VEC_SIZE-1:0][DATA_WIDTH-1:0]    i_frag_color,
+    input logic [1:0][DATA_WIDTH-1:0]             i_frag_tex_coord,
 
-    input logic [VEC_SIZE-1:0][DATA_WIDTH-1:0] i_texel_color,
+    input logic [VEC_SIZE-1:0][DATA_WIDTH-1:0]    i_texel_color,
 
     output logic o_pixel_valid,
-    output logic [VEC_SIZE-1:0][DATA_WIDTH-1:0] o_pixel_color
+    output logic signed [CORD_WIDTH-1:0]           o_pixel_x,
+    output logic signed [CORD_WIDTH-1:0]           o_pixel_y,
+    output logic [VEC_SIZE-1:0][DATA_WIDTH-1:0]    o_pixel_color
 );
 
     logic [VEC_SIZE-1:0][DATA_WIDTH-1:0] modulated_color;
@@ -32,10 +37,14 @@ module fragment_shader #(
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             o_pixel_valid <= 1'b0;
+            o_pixel_x     <= '0;
+            o_pixel_y     <= '0;
             o_pixel_color <= '0;
         end else begin
             o_pixel_valid <= i_frag_valid;
             if (i_frag_valid) begin
+                o_pixel_x     <= i_frag_x;
+                o_pixel_y     <= i_frag_y;
                 o_pixel_color <= modulated_color;
             end
         end
